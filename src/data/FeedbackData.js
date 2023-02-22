123ba46001e6e62bd19016857d1a0944c2c08469
piotrgorski23 <p.gorski@zdrojowahotels.pl>
2023-02-22 16:59:25 +0100

    React course by Traversy first commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime consequuntur reiciendis cum quidem dolores fugit laboriosam natus suscipit? Nobis fugiat earum quisquam sapiente dolores corrupti dolore expedita, error mollitia?',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime consequuntur reiciendis cum quidem dolores fugit laboriosam natus suscipit? Nobis fugiat earum quisquam sapiente dolores corrupti dolore expedita, error mollitia?',
+  },
+  {
+    id: 3,
+    rating: 6,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime consequuntur reiciendis cum quidem dolores fugit laboriosam natus suscipit? Nobis fugiat earum quisquam sapiente dolores corrupti dolore expedita, error mollitia?',
+  },
+]
+
+export default FeedbackData
